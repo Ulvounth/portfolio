004cfdbe6650e65b941d77b5ce1d17882babac1c
Andreas Ulvund <ulvund_a@hotmail.com>
2024-10-03 23:25:16 +0200

    Refactor ProjectCard component layout and styles

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -19,27 +19,31 @@ const ProjectCard: FC<ProjectProps> = ({
   github,
   live,
 }) => (
-  <div className="bg-background p-2 border border-white border-solid shadow-md rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 hover:border-blue-500">
-    {/* Make image and description area clickable */}
-    <Link href={`/projects/${id}`} className="block">
-      <div className="p-4">
-        <div className="relative w-full h-48 md:h-64 lg:h-72">
-          <Image
-            src={image}
-            alt={title}
-            fill
-            style={{ objectFit: "contain" }}
-            sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-          />
+  <div className="bg-background p-2 border border-white border-solid rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 hover:border-blue-500 flex flex-col">
+    {/* Content Area */}
+    <div className="flex-grow">
+      {/* Make image and description area clickable */}
+      <Link href={`/projects/${id}`} className="block h-full">
+        <div className="p-4">
+          <div className="relative w-full h-48 md:h-64 lg:h-72">
+            <Image
+              src={image}
+              alt={title}
+              fill
+              style={{ objectFit: "contain" }}
+              sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+              priority
+            />
+          </div>
+          <div>
+            <h2 className="text-xl font-semibold text-foreground">{title}</h2>
+            <p className="text-gray-300 mt-2">{description}</p>
+          </div>
         </div>
-        <div>
-          <h2 className="text-xl font-semibold text-foreground">{title}</h2>
-          <p className="text-gray-300 mt-2">{description}</p>
-        </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
     {/* Buttons */}
-    <div className="p-4 pt-0 flex space-x-4">
+    <div className="p-4 pt-0 flex justify-center space-x-4">
       <a
         href={github}
         className="inline-block px-4 py-2 bg-primary text-white rounded hover:bg-primaryDark transition-colors"
