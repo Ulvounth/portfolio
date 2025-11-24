81d9e31e7cd6c2f6b813a7608f82df71d35c1135
Andreas Ulvund <ulvund_a@hotmail.com>
2025-11-24 12:18:00 +0100

    style: format skill entries for consistency and readability

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -4,22 +4,74 @@ import Image from "next/image";
 const skills = [
   { name: "HTML", src: "/skill-icons/icons8-html.svg", needsBackground: false },
   { name: "CSS", src: "/skill-icons/icons8-css.svg", needsBackground: false },
-  { name: "JavaScript", src: "/skill-icons/icons8-javascript.svg", needsBackground: false },
-  { name: "React", src: "/skill-icons/icons8-react.svg", needsBackground: false },
-  { name: "Next.js", src: "/skill-icons/nextjs-icon-svgrepo-com.svg", needsBackground: true },
-  { name: "Tailwind", src: "/skill-icons/icons8-tailwind-css.svg", needsBackground: false },
-  { name: "Typescript", src: "/skill-icons/icons8-typescript-48.png", needsBackground: false },
-  { name: "Node.js", src: "/skill-icons/icons8-nodejs.svg", needsBackground: false },
+  {
+    name: "JavaScript",
+    src: "/skill-icons/icons8-javascript.svg",
+    needsBackground: false,
+  },
+  {
+    name: "React",
+    src: "/skill-icons/icons8-react.svg",
+    needsBackground: false,
+  },
+  {
+    name: "Next.js",
+    src: "/skill-icons/nextjs-icon-svgrepo-com.svg",
+    needsBackground: true,
+  },
+  {
+    name: "Tailwind",
+    src: "/skill-icons/icons8-tailwind-css.svg",
+    needsBackground: false,
+  },
+  {
+    name: "Typescript",
+    src: "/skill-icons/icons8-typescript-48.png",
+    needsBackground: false,
+  },
+  {
+    name: "Node.js",
+    src: "/skill-icons/icons8-nodejs.svg",
+    needsBackground: false,
+  },
   { name: "Git", src: "/skill-icons/icons8-git.svg", needsBackground: false },
   { name: "Jest", src: "/skill-icons/icons8-jest.png", needsBackground: false },
-  { name: "Figma", src: "/skill-icons/icons8-figma.svg", needsBackground: false },
+  {
+    name: "Figma",
+    src: "/skill-icons/icons8-figma.svg",
+    needsBackground: false,
+  },
   { name: "AWS", src: "/skill-icons/icons8-aws.svg", needsBackground: true },
-  { name: "Azure", src: "/skill-icons/icons8-azure-48.png", needsBackground: false },
-  { name: "Docker", src: "/skill-icons/icons8-docker.svg", needsBackground: false },
-  { name: "SQL", src: "/skill-icons/icons8-sql-48.png", needsBackground: false },
-  { name: "Express.js", src: "/skill-icons/icons8-express-js.svg", needsBackground: true },
-  { name: "MongoDB", src: "/skill-icons/icons8-mongodb.svg", needsBackground: false },
-  { name: "REST API", src: "/skill-icons/icons8-rest-api.png", needsBackground: false },
+  {
+    name: "Azure",
+    src: "/skill-icons/icons8-azure-48.png",
+    needsBackground: false,
+  },
+  {
+    name: "Docker",
+    src: "/skill-icons/icons8-docker.svg",
+    needsBackground: false,
+  },
+  {
+    name: "SQL",
+    src: "/skill-icons/icons8-sql-48.png",
+    needsBackground: false,
+  },
+  {
+    name: "Express.js",
+    src: "/skill-icons/icons8-express-js.svg",
+    needsBackground: true,
+  },
+  {
+    name: "MongoDB",
+    src: "/skill-icons/icons8-mongodb.svg",
+    needsBackground: false,
+  },
+  {
+    name: "REST API",
+    src: "/skill-icons/icons8-rest-api.png",
+    needsBackground: false,
+  },
 ];
 
 const Skills: React.FC = () => {
@@ -44,9 +96,7 @@ const Skills: React.FC = () => {
           >
             <div
               className={`${
-                skill.needsBackground
-                  ? "bg-white rounded-full p-3"
-                  : ""
+                skill.needsBackground ? "bg-white rounded-full p-3" : ""
               }`}
             >
               <Image
